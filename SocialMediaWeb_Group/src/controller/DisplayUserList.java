8f245fabdb0b7193c7d9a306791b3b960a485750
shruthi.shruthi <shruthi.shruthike@ymail.com>
2015-07-09 14:10:48 -0400

    Created DisplayUserList.java servlet and UserList.jsp to display list of users

---
+++ SocialMediaWeb_Group/src/controller/DisplayUserList.java
@@ -0,0 +1,77 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Dbuser;
+import mytools.DBUtil;
+
+/**
+ * Servlet implementation class DisplayUserList
+ */
+@WebServlet("/DisplayUserList")
+public class DisplayUserList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DisplayUserList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String nickname = request.getParameter("nickname");
+		String address = request.getParameter("address");
+		String favsong = request.getParameter("favsong");
+		String favcolor = request.getParameter("favcolor");
+		String favmovie = request.getParameter("favmovie");
+		Dbuser user = new Dbuser();
+		user.setPassword(password);
+		user.setNickname(nickname);
+		user.setName(name);
+		user.setAddress(address);
+		user.setEmail(email);
+		user.setFavcolor(favcolor);
+		user.setFavmovie(favmovie);
+		user.setFavsong(favsong);
+		
+		try{
+			em.persist(user);
+			List<Dbuser> userlist = em.createQuery("select c from Dbuser c").getResultList();
+			request.setAttribute("userlist", userlist);
+			
+		}catch(Exception e){
+			System.out.println("Exception in Display user servlet"+e.getMessage());
+		}finally{
+			em.close();
+		}
+		request.getRequestDispatcher("/UserList.jsp").forward(request, response);
+	}
+
+}
